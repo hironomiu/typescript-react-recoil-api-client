2e05e0814fc85f91c426ea3bf20470dce6f05bf8
hironomiu <hironomiu@gmail.com>
2022-02-04 22:32:29 +0000

    commited

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Suspense } from 'react'
+import { atom, RecoilRoot, selector } from 'recoil'
+import Main from './components/Main'
+
+export const userAtom = atom({
+  key: 'userAtom',
+  default: { email: 'taro@example.com', password: 'password' },
+})
+
+export const userEmailSelector = selector<{ email: string; password: string }>({
+  key: 'userSelector',
+  get: ({ get }) => {
+    const user = get(userAtom)
+    return user
+  },
+  set: ({ set }, newValue) => {
+    set(userAtom, newValue)
+  },
+})
+
+export const csrfTokenAtom = atom({
+  key: 'csrfTokenAtom',
+  default: '',
+})
+
+export const csrfTokenSelector = selector<string>({
+  key: 'csrfTokenSelector',
+  get: ({ get }) => {
+    const csrfToken = get(csrfTokenAtom)
+    return csrfToken
+  },
+  set: ({ set }, newValue) => {
+    console.log(newValue)
+    set(csrfTokenAtom, newValue)
+  },
+})
+
+export const isLoginAtom = atom({
+  key: 'isLoginAtom',
+  default: false,
+})
+
+export const isLoginSelector = selector<boolean>({
+  key: 'isLoginSelector',
+  get: ({ get }) => {
+    const isLogin = get(isLoginAtom)
+    return isLogin
+  },
+  set: ({ set }, newValue) => {
+    set(isLoginAtom, newValue)
+  },
+})
+
+const Fallback = () => {
+  return <div>Loading...</div>
+}
+
+const App = () => {
+  return (
+    <div>
+      <RecoilRoot>
+        <Suspense fallback={<Fallback />}>
+          <Main />
+        </Suspense>
+      </RecoilRoot>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Main.tsx
@@ -0,0 +1,72 @@
+import { FC, useEffect } from 'react'
+import { useRecoilState } from 'recoil'
+import {
+  userAtom,
+  userEmailSelector,
+  csrfTokenSelector,
+  isLoginSelector,
+} from '../App'
+
+const Main: FC = () => {
+  const [user, setUser] = useRecoilState(userEmailSelector)
+  const [csrfToken, setCsrfToken] = useRecoilState(csrfTokenSelector)
+  const [isLogin, setIslogin] = useRecoilState(isLoginSelector)
+
+  useEffect(() => {
+    ;(async () => {
+      const res = await fetch(
+        'https://localhost.hironomiu.com/api/v1/csrf-token'
+      )
+      const data = await res.json()
+      setCsrfToken(data.csrfToken)
+    })()
+  }, [])
+
+  if (isLogin) {
+    return <div>logined</div>
+  }
+
+  return (
+    <div>
+      <input
+        type="email"
+        value={user.email}
+        placeholder="email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <input
+        type="password"
+        value={user.password}
+        placeholder="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button
+        onClick={async (e) => {
+          e.preventDefault()
+          const res = await fetch(
+            'https://localhost.hironomiu.com/api/v1/auth/signin',
+            {
+              method: 'POST',
+              mode: 'cors',
+              cache: 'no-cache',
+              credentials: 'include',
+              redirect: 'follow',
+              headers: {
+                'Content-Type': 'application/json',
+                'CSRF-Token': csrfToken,
+              },
+              body: JSON.stringify({ ...user }),
+            }
+          )
+          // TODO ログインの判定
+          setIslogin(true)
+          console.log(res)
+        }}
+      >
+        ログイン
+      </button>
+    </div>
+  )
+}
+
+export default Main
