7ee13c149c018e8bff191866469a8cd0d94c2204
hironomiu <hironomiu@gmail.com>
2022-02-06 22:36:46 +0000

    commited

--- src/hooks/useMain.ts
+++ src/hooks/useMain.ts
@@ -1,15 +1,6 @@
 const API_URL = process.env.REACT_APP_API_URL
 
 export const useMain = () => {
-  // const fetchIsLogin = async () => {
-  //   const res = await fetch(API_URL + '/api/v1/auth/signin', {
-  //     method: 'GET',
-  //     credentials: 'include',
-  //   })
-  //   const data = await res.json()
-  //   return data
-  // }
-
   const fetchGetSignOut = async (csrfToken: string) => {
     const res = await fetch(API_URL + '/api/v1/auth/signout', {
       method: 'POST',
