a639abe8eab4b4c1d7d4b0db1702a98a3d58c06d
hironomiu <hironomiu@gmail.com>
2022-02-06 22:37:05 +0000

    commited

--- src/hooks/useMain.ts
+++ src/hooks/useMain.ts
@@ -16,27 +16,5 @@ export const useMain = () => {
     const data = await res.json()
     return data
   }
-
-  // type User = {
-  //   email: string
-  //   password: string
-  // }
-
-  // const fetchPostSignIn = async (csrfToken: string, user: User) => {
-  //   // TODO 型
-  //   const res: any = await fetch(API_URL + '/api/v1/auth/signin', {
-  //     method: 'POST',
-  //     mode: 'cors',
-  //     cache: 'no-cache',
-  //     credentials: 'include',
-  //     redirect: 'follow',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'CSRF-Token': csrfToken,
-  //     },
-  //     body: JSON.stringify({ ...user }),
-  //   })
-  //   return res
-  // }
   return { fetchGetSignOut }
 }
