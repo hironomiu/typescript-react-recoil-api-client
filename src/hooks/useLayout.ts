c2da66e2d6e169f8af74f5ddd2316b3e4dd0ac1a
hironomiu <hironomiu@gmail.com>
2022-02-17 00:24:47 +0000

    commited

--- src/hooks/useLayout.ts
+++ src/hooks/useLayout.ts
@@ -1,8 +1,35 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
+import { useRecoilValue, useRecoilState } from 'recoil'
+import { isLoginAtom } from '../recoil/global'
+import {
+  notificationCountSelector,
+  notificationSelector,
+} from '../recoil/notification'
+
 const FETCH_GET_IS_LOGIN = '/api/v1/auth/signin'
 const API_URL = new URL(FETCH_GET_IS_LOGIN, process.env.REACT_APP_API_URL)
 
+const FETCH_GET_NOTIFICATION = '/api/v1/notifications'
+const NOTIFICATION_API_URL = new URL(
+  FETCH_GET_NOTIFICATION,
+  process.env.REACT_APP_API_URL
+)
+
 export const useLayout = () => {
+  const isLogin = useRecoilValue(isLoginAtom)
+  const [, setNotificationCount] = useRecoilState(notificationCountSelector)
+  const [, setNotifications] = useRecoilState(notificationSelector)
+
+  const fetchGetNotification = useCallback(async () => {
+    const res = await fetch(NOTIFICATION_API_URL.toString(), {
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'include',
+    })
+    const data: Response = await res.json()
+    return data
+  }, [])
+
   const fetchGetIsLogin = useCallback(async () => {
     const res = await fetch(API_URL.toString(), {
       method: 'GET',
@@ -11,5 +38,19 @@ export const useLayout = () => {
     const data = await res.json()
     return data
   }, [])
+
+  useEffect(() => {
+    if (isLogin) {
+      ;(async () => {
+        // TODO 型
+        const data: any = await fetchGetNotification()
+        setNotificationCount(
+          data.data.filter((data: any) => data.is_confirmed === 0).length
+        )
+        setNotifications(data)
+      })()
+    }
+  }, [isLogin, setNotifications, setNotificationCount])
+
   return { fetchGetIsLogin }
 }
--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -2,47 +2,16 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useRecoilValue, useRecoilState } from 'recoil'
 import { isLoginAtom } from '../recoil/global'
-import {
-  notificationCountSelector,
-  notificationSelector,
-} from '../recoil/notification'
-
-const FETCH_GET_NOTIFICATION = '/api/v1/notifications'
-const API_URL = new URL(FETCH_GET_NOTIFICATION, process.env.REACT_APP_API_URL)
+import { notificationSelector } from '../recoil/notification'
 
 export const useNotification = () => {
   const navigate = useNavigate()
   const isLogin = useRecoilValue(isLoginAtom)
-
-  const [notifications, setNotifications] = useRecoilState(notificationSelector)
-  const [, setNotificationCount] = useRecoilState(notificationCountSelector)
+  const [notifications] = useRecoilState(notificationSelector)
 
   useEffect(() => {
     if (!isLogin) navigate('/auth')
   }, [navigate, isLogin])
 
-  useEffect(() => {
-    if (isLogin) {
-      ;(async () => {
-        // TODO 型
-        const data: any = await fetchGetNotification()
-        // TODO Main読み込み時に設定する
-        setNotificationCount(
-          data.data.filter((data: any) => data.is_confirmed === 0).length
-        )
-        setNotifications(data)
-      })()
-    }
-  }, [isLogin, setNotifications, setNotificationCount])
-
-  const fetchGetNotification = async () => {
-    const res = await fetch(API_URL.toString(), {
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'include',
-    })
-    const data: Response = await res.json()
-    return data
-  }
-  return { notifications, fetchGetNotification }
+  return { notifications }
 }
