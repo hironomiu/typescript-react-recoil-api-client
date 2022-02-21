8f3b882494e20fdcf4b31030ea053d85a3564c9f
hironomiu <hironomiu@gmail.com>
2022-02-21 09:23:58 +0000

    commited

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,34 +1,12 @@
 import { memo, useState } from 'react'
 import { useNotification } from '../hooks/useNotification'
-import { csrfTokenSelector } from '../recoil/global'
-import { useRecoilState } from 'recoil'
 import NotificationMessageModal from './modal/NotificationMessageModal'
 
-const FETCH_PUT_NOTIFICATION = '/api/v1/notifications'
-const NOTIFICATION_API_URL = new URL(
-  FETCH_PUT_NOTIFICATION,
-  process.env.REACT_APP_API_URL
-)
-
 const Notification = memo(() => {
-  const { notifications } = useNotification()
+  const { notifications, updateIsConfirmed } = useNotification()
   const [modalOn, setModalOn] = useState(false)
   const [index, setIndex] = useState(0)
-  const [csrfToken] = useRecoilState(csrfTokenSelector)
 
-  const updateIsConfirmed = (id: number) => {
-    console.log(csrfToken)
-
-    fetch(NOTIFICATION_API_URL.toString(), {
-      method: 'PUT',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        'CSRF-Token': csrfToken,
-      },
-      body: JSON.stringify({ id: id }),
-    })
-  }
   return (
     <div className="flex flex-col h-[86vh] justify-center items-center">
       <h1 className="text-2xl mb-10">Notification</h1>
--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,17 +1,35 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useRecoilValue, useRecoilState } from 'recoil'
-import { isLoginAtom } from '../recoil/global'
+import { isLoginAtom, csrfTokenSelector } from '../recoil/global'
 import { notificationSelector } from '../recoil/notification'
 
+const FETCH_PUT_NOTIFICATION = '/api/v1/notifications'
+const NOTIFICATION_API_URL = new URL(
+  FETCH_PUT_NOTIFICATION,
+  process.env.REACT_APP_API_URL
+)
 export const useNotification = () => {
   const navigate = useNavigate()
+  const [csrfToken] = useRecoilState(csrfTokenSelector)
   const isLogin = useRecoilValue(isLoginAtom)
   const [notifications] = useRecoilState(notificationSelector)
+
+  const updateIsConfirmed = (id: number) => {
+    fetch(NOTIFICATION_API_URL.toString(), {
+      method: 'PUT',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        'CSRF-Token': csrfToken,
+      },
+      body: JSON.stringify({ id: id }),
+    })
+  }
 
   useEffect(() => {
     if (!isLogin) navigate('/auth')
   }, [navigate, isLogin])
 
-  return { notifications }
+  return { notifications, updateIsConfirmed }
 }
